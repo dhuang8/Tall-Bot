a48b54a361067c33d483c512a206c5e9ca65e48b
unknown <github@dhuang8.com>
2020-10-15 19:49:05 -0400

    provinces fix

--- discordbot.js
+++ discordbot.js
@@ -2791,7 +2791,20 @@ rp({
     })
 })
 
-
+let covid_provinces = {"Alberta":"AB",
+    "British Columbia":"BC",
+    "Manitoba":"MB",
+    "New Brunswick":"NB",
+    "Newfoundland and Labrador":"NL",
+    "Northwest Territories":"NT",
+    "Nova Scotia":"NS",
+    "Nunavut":"NU",
+    "Ontario":"ON",
+    "Prince Edward Island":"PE",
+    "Quebec":"QC",
+    "Saskatchewan":"SK",
+    "Yukon":"YT",
+    "Repatriated Travellers":"RT"};
 
 commands.push(new Command({
     name: "covid",

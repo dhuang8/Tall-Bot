34cc2282839e8bcb7a19e8458be5d6d52e582775
me@dhuang8.com <me@dhuang8.com>
2023-07-14 09:37:30 +0000

    interaction reply fix

--- index.js
+++ index.js
@@ -61,16 +61,16 @@ client.on(Events.InteractionCreate, async (interaction) => {
         try {
             const response = await command.execute(interaction);
             if (typeof response == "string") {
-                interaction.reply(response).catch(console.error);
+                interaction.editReply(response).catch(console.error);
             } else {
-                interaction.reply(response).catch(console.error);
+                interaction.editReply(response).catch(console.error);
             }
         } catch (error) {
             console.error(error);
             if (interaction.replied || interaction.deferred) {
-                await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+                await interaction.editReply({ content: 'There was an error while executing this command!', ephemeral: true });
             } else {
-                await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+                await interaction.editReply({ content: 'There was an error while executing this command!', ephemeral: true });
             }
         }
     } else if (interaction.isButton()) {
